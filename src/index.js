46cbf169cf4f4a29005d204948741f4dcc55d9d8
Talha Dar <talhadar2011@gmail.com>
2021-03-07 18:22:35 +0100

    Final

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+
 import reportWebVitals from './reportWebVitals';
+import Main from './component/main.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
